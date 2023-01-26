d00bbcf22caf43dc934cec195bcefa8dd0ff3882
dakshinatharindu <tharinduokd@gmail.com>
2023-01-26 10:26:02 +0530

    created reg file

---
+++ src/main/scala/RegisterFile.scala
@@ -0,0 +1,30 @@
+package riscv
+
+import chisel3._
+import chisel3.util._
+import riscv.shared.Constants._
+
+class RegisterFile extends Module {
+  val io = IO(new Bundle {
+    val r_addr1 = Input(UInt(5.W))
+    val r_addr2 = Input(UInt(5.W))
+    val w_addr = Input(UInt(5.W))
+    val w_data = Input(SInt(32.W))
+    val w_en = Input(Bool())
+    val r_data1 = Output(SInt(32.W))
+    val r_data2 = Output(SInt(32.W))
+  })
+
+  val register_file = Reg(Vec(32, SInt(32.W)))
+  io.r_data1 := register_file(io.r_addr1)
+  io.r_data2 := register_file(io.r_addr2)
+
+  when (io.w_en){
+    register_file(io.w_addr) := io.w_data
+  }
+}
+
+object RegisterFile extends App {
+  val s = getVerilogString(new RegisterFile())
+  println(s)
+}
