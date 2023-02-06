4ada68f2f48934679b346339661a721aa4dc3e0a
dakshinatharindu <tharinduokd@gmail.com>
2023-02-07 01:17:07 +0530

    tested processor with rv32i toolchain

--- src/test/scala/ImmGenTest.scala
+++ src/test/scala/ImmGenTest.scala
@@ -9,8 +9,8 @@ import riscv.shared.Constants._
 class ImmGenTest extends AnyFlatSpec with ChiselScalatestTester {
   "ImmGenTest" should "pass" in {
     test(new ImmGen) { dut =>
-      dut.io.in.poke("b001010101010_00000_000_00000_1100111".U)
-      dut.io.format.poke(5.U)
+      dut.io.in.poke("h024000ef".U)
+      dut.io.format.poke(J.U)
       println(dut.io.out.peek())
     }
   }
