910a6fdb32bc3b869576737fb60f0176500eb8f7
dakshinatharindu <tharinduokd@gmail.com>
2023-05-04 00:10:22 +0530

    change instruction memory address to byte addressable

--- src/test/scala/CoreTest.scala
+++ src/test/scala/CoreTest.scala
@@ -12,8 +12,6 @@ class CoreTest extends AnyFlatSpec with ChiselScalatestTester {
     test(new Core) { dut =>
       dut.io.instr.poke("hfe010113".U)
       dut.clock.step()
-      dut.io.instr.poke("h00112e23".U)
-      dut.clock.step()
       println("ALUOut " + dut.io.ALUOut.peek().toString)
       println("memRead " + dut.io.memRead.peek().toString)
       println("memWrite " + dut.io.memWrite.peek().toString)
